7da44caa30ffc08cf59ac9acd290964447e2b7df
Spencer Atkins-Letten <sjal500@york.ac.uk>
2018-11-30 09:42:59 +0000

    Just testing this works, made a couple of changes just to make it work for me

---
+++ Project/core/src/com/seaofgeese/game/MainMenuScreen.java
@@ -0,0 +1,40 @@
+package com.seaofgeese.game;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen {
+    @java.lang.Override
+    public void show() {
+
+    }
+
+    @java.lang.Override
+    public void render(float delta) {
+
+    }
+
+    @java.lang.Override
+    public void resize(int width, int height) {
+
+    }
+
+    @java.lang.Override
+    public void pause() {
+
+    }
+
+    @java.lang.Override
+    public void resume() {
+
+    }
+
+    @java.lang.Override
+    public void hide() {
+
+    }
+
+    @java.lang.Override
+    public void dispose() {
+
+    }
+}
--- Project/desktop/src/com/seaofgeese/game/desktop/DesktopLauncher.java
+++ Project/desktop/src/com/seaofgeese/game/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package com.seaofgeese.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.seaofgeese.game.SeaOfGeese;
+import com.seaofgeese.game.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SeaOfGeese(), config);
+		config.foregroundFPS = 60;
+		new LwjglApplication(new Game(), config);
 	}
 }
