5d3914863f06421e1d390abd09cc266243b6865f
Spencer Atkins-Letten <sjal500@york.ac.uk>
2018-12-07 23:49:59 +0000

    Reworking how the screens work and also creating classes for future screens

--- Project/core/src/com/seaofgeese/game/MainGame.java
+++ Project/core/src/com/seaofgeese/game/MainGame.java
@@ -2,30 +2,44 @@ package com.seaofgeese.game;
 
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 
 public class MainGame extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
 
-		this.batch = new SpriteBatch();
-		this.setScreen(new MainMenuScreen(this));
+	private LoadingScreen loadingScreen;
+	private MenuScreen menuScreen;
+	private MainScreen mainScreen;
+	private EndScreen endScreen;
+	private LeaderboardScreen leaderboardScreen;
+
+	public final static int MENU = 0;
+	public final static int LEADERBOARD = 1;
+	public final static int APPLICATION = 2;
+	public final static int ENDGAME = 3;
+
+
+	@Override
+	public void create() {
+		loadingScreen = new LoadingScreen(this);
+		setScreen(loadingScreen);
 	}
 
-	@Override
-	public void render () {
-		super.render();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		//img.dispose();
+	public void changeScreen(int screen){
+		switch(screen){
+			case MENU:
+				if(menuScreen == null) menuScreen = new MenuScreen(this);
+				this.setScreen(menuScreen);
+				break;
+			case LEADERBOARD:
+				if(leaderboardScreen == null) leaderboardScreen = new LeaderboardScreen(this);
+				this.setScreen(leaderboardScreen);
+				break;
+			case APPLICATION:
+				if(mainScreen == null) mainScreen = new MainScreen(this);
+				this.setScreen(mainScreen);
+				break;
+			case ENDGAME:
+				if(endScreen == null) endScreen = new EndScreen(this);
+		}
 	}
 }
---
+++ Project/core/src/com/seaofgeese/game/MenuScreen.java
@@ -0,0 +1,46 @@
+package com.seaofgeese.game;
+
+
+import com.badlogic.gdx.Screen;
+
+
+public class MenuScreen implements Screen {
+    private MainGame parent;
+    public MenuScreen(MainGame mainGame){
+        parent = mainGame;
+    }
+    @java.lang.Override
+    public void show() {
+
+    }
+
+    @java.lang.Override
+    public void render(float delta) {
+
+    }
+
+    @java.lang.Override
+    public void resize(int width, int height) {
+
+    }
+
+    @java.lang.Override
+    public void pause() {
+
+    }
+
+    @java.lang.Override
+    public void resume() {
+
+    }
+
+    @java.lang.Override
+    public void hide() {
+
+    }
+
+    @java.lang.Override
+    public void dispose() {
+
+    }
+}
