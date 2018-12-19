a5cd313441f3fcc13e5bba697fb40900da0552ad
Benjampire <bjh551@york.ac.uk>
2018-12-19 13:41:20 +0000

    Almost finished the Quest class

--- Project/core/src/com/seaofgeese/game/Quest.java
+++ Project/core/src/com/seaofgeese/game/Quest.java
@@ -1,12 +1,10 @@
 package com.seaofgeese.game;
 
-import com.badlogic.gdx.Gdx;
-
-public class Quest {
+class Quest {						//TODO you made this class package private, learn what that will mean for this
 
 	private String questTitle;		//e.g. Take the Sails
 	private String questDesc;		//Sink 5 pirate ships
-	private int questID				//Simple number system, 
+	private int questID;				//Simple number system,
 	private int goldReward;			//e.g. 600 gold
 	private int pointsReward;		//e.g. 300 points
 	private int targetAmount;		//8       -Ships destroyed
@@ -17,16 +15,103 @@ public class Quest {
 	private float returnLocation;
 	private float targetLocation;
 
-	private ArrayList<int> completedQuests;
-	private ArrayList<int> activeQuests;
-	private ArrayList<int> unbegunQuests;
-
-    public String getQuestTitle() {
-		return QuestTitle;
+	public Quest(String questTitle, String questDesc, int questID, int goldReward, int pointsReward, int targetAmount, int currentAmount, boolean isComplete, boolean isRepeatable, float returnLocation, float targetLocation) {
+		this.questTitle = questTitle;
+		this.questDesc = questDesc;
+		this.questID = questID;
+		this.goldReward = goldReward;
+		this.pointsReward = pointsReward;
+		this.targetAmount = targetAmount;
+		this.currentAmount = currentAmount;
+		this.isComplete = isComplete;
+		this.isRepeatable = isRepeatable;
+		this.returnLocation = returnLocation;
+		this.targetLocation = targetLocation;
 	}
 
-	public void setQuestTitle(String title) {
-		this.questTitle = title;
+	//TODO Do we even need half of the setters? we need the getters sure, but most just have initially set values, no?
+
+	public String getQuestTitle() { return this.questTitle; }
+
+	public void setQuestTitle(String questTitle) {
+		this.questTitle = questTitle;
 	}
 
+	public String getQuestDesc() {
+		return this.questDesc;
+	}
+
+	public void setQuestDesc(String questDesc) {
+		this.questDesc = questDesc;
+	}
+
+	public int getQuestID() {
+		return this.questID;
+	}
+
+	public void setQuestID(int questID) {
+		this.questID = questID;
+	}
+
+	public int getGoldReward() {
+		return this.goldReward;
+	}
+
+	public void setGoldReward(int goldReward) {
+		this.goldReward = goldReward;
+	}
+
+	public int getPointsReward() {
+		return this.pointsReward;
+	}
+
+	public void setPointsReward(int pointsReward) {
+		this.pointsReward = pointsReward;
+	}
+
+	public int getTargetAmount() {
+		return this.targetAmount;
+	}
+
+	public void setTargetAmount(int targetAmount) {
+		this.targetAmount = targetAmount;
+	}
+
+	public int getCurrentAmount(){return this.currentAmount;}
+
+	public void setCurrentAmount(int currentAmount) {  //TODO we set it initially sure, but technically we only need to increment it after a battle for this current system
+		this.currentAmount = currentAmount;
+	}
+
+	public boolean getIsComplete() {
+		return this.isComplete;
+	}
+
+	public void setIsComplete(Boolean isComplete) {
+		this.isComplete = isComplete;
+	}
+
+	public boolean getIsRepeatable() {
+		return this.isRepeatable;
+	}
+
+	public void setIsRepeatable(Boolean isRepeatable) {
+		this.isRepeatable = isRepeatable;
+	}
+
+	public float getReturnLocation() {
+		return this.returnLocation;
+	}
+
+	public void setReturnLocation(float returnLocation) {
+		this.returnLocation = returnLocation;
+	}
+
+	public float getTargetLocation() {
+		return this.targetLocation;
+	}
+
+	public void setTargetLocation(float targetLocation) {
+		this.targetLocation = targetLocation;
+	}
 }
--- Project/core/src/com/seaofgeese/game/QueststateController.java
+++ Project/core/src/com/seaofgeese/game/QueststateController.java
@@ -1,5 +1,21 @@
 package com.seaofgeese.game;
 
 import com.badlogic.gdx.Gdx;
+import java.util.ArrayList;
 
-public class QuestProperties {}
+public class QueststateController {
+
+
+    private ArrayList<Integer> completedQuests;
+    private ArrayList<Integer> activeQuests;
+    private ArrayList<Integer> unbegunQuests;
+
+    //TODO INIT function to set up quest instances
+    //TODO code to load quests into unbegun quests with the starter quest loaded into activeQuests
+    //TODO code gameplay features, like how it functions, calling from ID and setting it up.
+    //TODO code dependencies - Make the HashSet? or some other hash thing
+
+
+    //TODO set up isComplete to change if CurrentAmount hits target amount and once it's complete we don't need to increment it any more
+
+}
